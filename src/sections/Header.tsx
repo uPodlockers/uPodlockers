803f492d921067432163978c289631e8a396c9fc
satvik0805 <satvik.agg@gmail.com>
2024-10-20 02:07:09 +0800

    Replaced existing code with light SAAS template

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,40 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from '@/assets/arrow-right.svg';
+import Logo from '@/assets/logo-upod.png';
+import Image from "next/image";
+import MenuIcon from '@/assets/menu.svg';
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-small">
+        <div className="inline-flex gap-1 items-center">
+          <p>24*7 Accessible Parcel Lockers at your Service</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div>
+        <div className="py-5">
+          <div className="container">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-3">
+                <Image src={Logo} alt="uPOD Logo" height={150} width={150} />
+                {/* <span className="font-bold text-lg">upod</span> */}
+              </div>
+
+              <MenuIcon className="h-5 w-5 md:hidden" />
+              
+              <nav className="hidden md:flex gap-6 font-semibold text-black/100 items-center">
+                <a href="#">Home</a>
+                <a href="#">Products</a>
+                <a href="#">Solutions</a>
+                <a href="#">Company</a>
+                <a href="#">Resources</a>
+                <a href="#">Contact Us</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,19 @@
 export const Hero = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div>
+          <div className="text-small inline-flex border border-color-[#222]/10 px-3 py-1 rouned-lg tracking-tight">
+            Now available nationwide!
+          </div>
+          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+            Building Indias First Agnostic Smart Parcel Locker Network
+          </h1>
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+          Designed for businesses, communities, and individuals looking for a hassle-free parcel solution.
+          </p>  
+        </div>
+      </div>
+    </section>
+  );
 };
