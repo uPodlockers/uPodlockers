46933f2e37fcc7afcf9143785da18eef5d5680e7
uPodlockers <upodlockers@gmail.com>
2025-03-19 18:36:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,45 +1,29 @@
-/**
- * @type {import('next').NextConfig}
- */
-const isProd = process.env.NODE_ENV === "production";
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'export',
-  reactStrictMode: true,
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
-  distDir: "dist",
-
-  assetPrefix: isProd ? "/uPodlockers/" : "", // Use your repo name here
-  basePath: isProd ? "/uPodlockers" : "", // Use your repo name here
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
