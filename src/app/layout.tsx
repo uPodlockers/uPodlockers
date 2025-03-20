04ad55442dcc996de00d6163637bd09fc598f90b
uPodlockers <upodlockers@gmail.com>
2025-03-20 23:52:57 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,13 @@
 'use client';
 
-import { DM_Sans } from "next/font/google"; // Import font
-import clsx from "clsx"; // clsx for conditionally combining class names
-import Navbar from "@/components/Navbar"; // Import global Navbar
-import { Footer } from "@/sections/Footer"; // Import global Footer
-import "./globals.css"; // Global styles
-
-import { Metadata } from 'next'
+import { DM_Sans } from "next/font/google";
+import clsx from "clsx";
+import Navbar from "@/components/Navbar";
+import { Footer } from "@/sections/Footer";
+import "./globals.css";
 
 // Applying the DM_Sans font with latin subset
 const dmSans = DM_Sans({ subsets: ["latin"] });
-
-// Define the page metadata, used for SEO, title, etc.
-export const metadata: Metadata = {
-  title: "upod smart lockers",
-  description:
-    "Solving last-mile delivery issues by making affordable smart parcel lockers",
-};
 
 export default function RootLayout({
   children,
@@ -25,8 +16,8 @@ export default function RootLayout({
     <html lang="en" className="relative">
       <head>
         {/* Here you can inject head tags dynamically, e.g., title */}
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>UPOD smart lockers</title>
+        <meta name="description" content="Solving last-mile delivery issues by making affordable smart parcel lockers" />
       </head>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         <Navbar /> {/* Global Navbar */}
