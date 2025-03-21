855c1d044118cdf3ad28b117d813a053da12b234
uPodlockers <upodlockers@gmail.com>
2025-03-21 22:53:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,44 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: 'export',
-  basePath: '/nextjs-github-pages',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  experimental: {
-    serverComponents: true,
+  distDir: "dist",
+
+  assetPrefix: isProd ? "/uPodlockers/" : "", // Use your repo name here
+  basePath: isProd ? "/uPodlockers" : "", // Use your repo name here
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
